c2cd3d0c6d2a5185a5e3f7a56bfa7ea0ab1805c8
jesusgabriel9696@gmail.com <jesusgabriel9696@gmail.com>
2024-05-11 17:58:50 -0400

    condicionales

---
+++ Condicionales/if.py
@@ -0,0 +1,24 @@
+
+
+edad1= int(input("Ingresa la edad de la primera persona"))
+"""edad2= int(input("Ingresa la edad de la segunda persona"))"""
+
+"""if edad1<edad2:
+
+    print("La persona 1 es menor que la persona 2")
+
+if edad1<edad2:
+
+    print("La persona 2 es mayor que la persona 1")
+
+else:
+    print("Tienen la misma edad")
+
+if edad1 == edad2:
+    print("Tienen la misma edad")"""
+
+if edad1 < 18:
+   print("No puedes pasar a la discoteca")
+
+else:
+   print("Sí puedes pasar a la discoteca")
---
+++ Condicionales/oylogicos.py
@@ -0,0 +1,10 @@
+
+
+
+edad= int(input("Cuál es tu edad: "))
+hora= int(input("Dime la hora: "))
+
+if edad<18 and hora<8:
+    
+    print("ERES MENOR DE EDAD Y TODAVÍA NO SON LAS 8")
+
