2d3877337bf01d19ba3a8a140188c9bd4f48bcd3
jesusgabriel9696@gmail.com <jesusgabriel9696@gmail.com>
2024-05-11 16:50:16 -0400

    clases 2

---
+++ 02operarimetica.py
@@ -0,0 +1,13 @@
+from variables import numero1
+from variables import entero
+
+"""suma= numero1+entero
+print(suma)"""
+
+numero2= input("INGRESA UN NUMERO: ")
+numero3= int(input("INGRESA UN SEGUNDO NÙMERO: "))
+
+suma= numero1 + numero3
+
+print(suma)
+
---
+++ variables.py
@@ -0,0 +1,16 @@
+
+numero1=12
+
+entero=10
+
+decimal=10.5
+
+texto="HOLA"
+
+print(entero)
+print(decimal)
+print(texto)
+
+print(type(entero))
+print(type(decimal))
+print(type(texto))
